b00b76244101b5f83cba81db2205cf095e2e8b68
oleksandr.o.morozov <morozovaleks@ukr.net>
2022-02-03 23:11:32 +0200

    add images to drawable create splash and main screen and navigation

--- app/src/main/java/com/example/myweatherforecast/di/AppModule.kt
+++ app/src/main/java/com/example/myweatherforecast/di/AppModule.kt
@@ -1,8 +1,13 @@
 package com.example.myweatherforecast.di
 
 import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 
 
 @Module
+@InstallIn((SingletonComponent::class))
 class AppModule {
+
+
 }
