96b067af3d60bf7eb3c3bbdd73dd66457231a975
oleksandr.o.morozov <morozovaleks@ukr.net>
2022-03-02 00:01:44 +0200

    create weather db and dao

---
+++ app/src/main/java/com/example/myweatherforecast/data/WeatherDao.kt
@@ -0,0 +1,26 @@
+package com.example.myweatherforecast.data
+
+import androidx.room.*
+import com.example.myweatherforecast.model.Favorite
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface WeatherDao {
+    @Query("SELECT * from fav_tbl")
+    fun getFavorites(): Flow<List<Favorite>>
+
+    @Query("SELECT * from fav_tbl where city =:city")
+    suspend fun getFavById(city: String): Favorite
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavorite(favorite: Favorite)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateFavorite(favorite: Favorite)
+
+    @Query("DELETE from fav_tbl")
+    suspend fun deleteAllFavorites()
+
+    @Delete
+    suspend fun deleteFavorite(favorite: Favorite)
+}
--- app/src/main/java/com/example/myweatherforecast/di/AppModule.kt
+++ app/src/main/java/com/example/myweatherforecast/di/AppModule.kt
@@ -1,10 +1,15 @@
 package com.example.myweatherforecast.di
 
+import android.content.Context
+import androidx.room.Room
+import com.example.myweatherforecast.data.WeatherDao
+import com.example.myweatherforecast.data.WeatherDatabase
 import com.example.myweatherforecast.network.WeatherApi
 import com.example.myweatherforecast.utils.Constants.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -14,6 +19,23 @@ import javax.inject.Singleton
 @Module
 @InstallIn((SingletonComponent::class))
 class AppModule {
+
+    @Singleton
+    @Provides
+    fun provideWeatherDao(weatherDatabase: WeatherDatabase):WeatherDao
+    = weatherDatabase.weatherDao()
+
+    @Singleton
+    @Provides
+    fun provideAppDatabase(@ApplicationContext
+                           context: Context
+    ):WeatherDatabase =
+        Room.databaseBuilder(
+            context,WeatherDatabase::class.java,
+            "weather_database"
+        ).fallbackToDestructiveMigration()
+            .build()
+
 
     @Provides
     @Singleton
